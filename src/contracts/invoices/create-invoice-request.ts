56d01ae463fc6dfb0cc42ba5c74151bddd5ba2f4
Diogo Assunção <dassuncao1234@gmail.com>
2024-01-17 00:27:50 +0000

    add: Create invoice

--- src/contracts/invoices/create-invoice-request.ts
+++ src/contracts/invoices/create-invoice-request.ts
@@ -1,5 +1,8 @@
 interface CreateInvoiceRequest {
     buyercustomerparty: string;
+    serie: number;
+    documentDate: string;
+    postingDate: string;
     documentLines: [{
         salesItem: string;
         unitPrice: {
--- src/routes/invoices.ts
+++ src/routes/invoices.ts
@@ -1,26 +1,47 @@
 import { FastifyInstance } from "fastify";
 import jasminApi from "../services/jasmin-api";
 
+type InvoiceRequest = {
+    customerId: string,
+    products : [
+        {
+            productId: string,
+            amount: number
+        }
+    ]
+}
+
 export default async function name(app: FastifyInstance, opts) {
+    function convertDate(date: Date) {
+        date.setFullYear(2023);
+        return date.toISOString().slice(0, 19).replace("T", " ");
+    }
+
     app.post("/invoices", async (request, reply) => {
-        let response;
-        
-        try {
-            response = await jasminApi.post("/billing/invoices", request.body as CreateInvoiceRequest);
+        const req = request.body as InvoiceRequest;
 
-            if (response.status !== 201) {
-                throw new Error("Error creating an invoice");
-            }
+        const documentDate = new Date();
+        const postingDate = new Date();
+        postingDate.setDate(documentDate.getDate() + 30);
 
-        } catch (error) {
-            console.log(error);
-            
+        const { status, data } = await jasminApi.post("/billing/invoices", {
+            buyercustomerparty: req.customerId,
+            serie: 2023,
+            documentDate: "2023-12-17 00:16:44",
+            postingDate: "2023-12-26 00:16:44",
+            documentLines: req.products.map(product => ({
+                salesItem: product.productId,
+                unitPrice: {
+                    amount: product.amount
+                }
+            }))
+        } as CreateInvoiceRequest);
+
+        if (status !== 201) {
             throw new Error("Error creating an invoice");
         }
 
-        return {
-            invoiceId: response.data
-        } as CreateInvoiceResponse;
+        return data;
     });
 
     app.get("/invoices/:invoiceId", async (request, reply) => {
