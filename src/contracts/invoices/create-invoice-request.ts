fc56f15b90c94da8164a26059af9ac4f0a43231d
Diogo Assunção <dassuncao1234@gmail.com>
2024-01-19 20:29:45 +0000

    modify: Add the real quantity to products

--- src/contracts/invoices/create-invoice-request.ts
+++ src/contracts/invoices/create-invoice-request.ts
@@ -1,12 +1,14 @@
 interface CreateInvoiceRequest {
     buyercustomerparty: string;
     serie: number;
+    discount: number;
     documentDate: string;
     postingDate: string;
     documentLines: [{
         salesItem: string;
+        quantity: number;
         unitPrice: {
             amount: number;
         };
-    }];
+    }]
 };
--- src/routes/invoices.ts
+++ src/routes/invoices.ts
@@ -6,9 +6,11 @@ type InvoiceRequest = {
     products : [
         {
             productId: string,
-            amount: number
+            amount: number,
+            quantity: number
         }
-    ]
+    ],
+    discount: number
 }
 
 export default async function name(app: FastifyInstance, opts) {
@@ -24,13 +26,18 @@ export default async function name(app: FastifyInstance, opts) {
         const postingDate = new Date();
         postingDate.setDate(documentDate.getDate() + 30);
 
+        const totalAmount = req.products.reduce((prev, current) => prev + current.amount * current.quantity, 0);
+        const discountPercent = req.discount * 100 / totalAmount;
+
         const { status, data } = await jasminApi.post("/billing/invoices", {
             buyercustomerparty: req.customerId,
             serie: 2023,
+            discount: discountPercent,
             documentDate: "2023-12-17 00:16:44",
             postingDate: "2023-12-26 00:16:44",
             documentLines: req.products.map(product => ({
                 salesItem: product.productId,
+                quantity: product.quantity,
                 unitPrice: {
                     amount: product.amount
                 }
